25286b5cbacdd14a7049bcbb2ba5179e6ca63bbd
akashy0752016 <akashy075@gmail.com>
2024-08-08 20:23:06 +0530

    Update App.js

--- demo/src/App.js
+++ demo/src/App.js
@@ -9,19 +9,85 @@ import {
 import Login from "./pages/Login";
 import Home from './pages/Home';
 import ObjectDirectory from './pages/ObjectDirectory';
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'; 
+import RouteApp from './components/common/route';
+const CLIENT_ID = 'Ov23liL5MFCO0aHzPn5o';
 
 function App() {
+  const [rerender, setRerender] = useState(false);
+  const [userData, setUserData] = useState({});
+  const [isLogedIn,setLogedIn]=useState(false)
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get("code");
+    console.log("code :"+ codeParam);
+
+    if(codeParam && (localStorage.getItem('accessToken') === null)) {
+      async function getAccessToken() {
+        await fetch('http://localhost:4000/getAccessToken?code=' + codeParam, {
+          method: 'GET'
+        }).then((response) => {
+          return response.json();
+        }).then((data) => {
+            console.log(data);
+          if (data.access_token) {
+            getUserData();
+            if(
+              Object.keys(userData).length !== 0 ){
+              localStorage.setItem('userName', userData.login);
+            }
+                
+            
+              localStorage.setItem('accessToken', data.access_token);
+              setLogedIn(true)
+              window.location.href = "http://localhost:3000/home";
+              //setRerender(!rerender);
+            }
+        });
+      }
+      getAccessToken();
+    }
+  }, []);
+
+  async function getUserData() {
+    await fetch('http://localhost:4000/getUserData', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((data) => {
+        console.log(data);
+        setUserData(data);
+    });
+  }
+
+  function loginWithGitHub() {
+    window.location.assign("https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID);
+  }
+
   return ( 
-    <> 
-      <Router> 
-        <Routes> 
-          <Route path="/Home" element={<Home />} /> 
-          <Route path="/Object/:objectId" element={<ObjectDirectory />} />
-        </Routes> 
-      </Router> 
-    </> 
-  );
+  <header className="App-header">
+    
+    <>
+      <RouteApp/>
+        {!localStorage.getItem('accessToken') &&
+          <>
+        <h3>User not logged in</h3>
+            <button onClick={loginWithGitHub}>
+              Login with GitHub
+          </button>
+          </>
+        }
+        
+            
+          
+    </>
+  </header>
+);
 }
 
 export default App;
