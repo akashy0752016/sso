4792fc35b1e0264e97b7ac08dbfe3f3dacb7d806
akashy0752016 <akashy075@gmail.com>
2024-08-09 14:15:23 +0530

    Update Sidebar.js

--- demo/src/components/common/Sidebar.js
+++ demo/src/components/common/Sidebar.js
@@ -1,26 +1,26 @@
 import React from "react";
- 
+import ConfigJson from "../../config.json";
+
+const githubData = ConfigJson.data;
 const Sidebar = () => {
     return (
         <aside id="sidebar" className="sidebar">
 
             <ul className="sidebar-nav" id="sidebar-nav">
-
+            
             <li className="nav-item">
                 <a className="nav-link " data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
                 <i className="bi bi-layout-text-window-reverse"></i><span>Directory</span><i className="bi bi-chevron-down ms-auto"></i>
                 </a>
                 <ul id="tables-nav" className="nav-content collapse show" data-bs-parent="#sidebar-nav">
-                <li>
-                    <a href="./1" className="active">
-                    <i className="bi bi-circle"></i><span>General Tables</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="./2">
-                    <i className="bi bi-circle"></i><span>Data Tables</span>
-                    </a>
-                </li>
+                
+                {githubData.map((gh, i)=>(
+                    <li>
+                        <a href={ window.location.origin + "/object/" +gh.id }>
+                        <i className="bi bi-circle"></i><span>{ gh.title }</span>
+                        </a>
+                    </li>
+                ))}
                 </ul>
             </li>
 
