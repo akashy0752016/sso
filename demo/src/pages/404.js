5da925f8139c97547b2d1e567ad2f54ade3f043e
Akash Yadav <akashy075@gmail.com>
2024-08-11 05:35:57 +0530

    Update scripts

--- demo/src/pages/404.js
+++ demo/src/pages/404.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Page404 = () => {
     return <>
-        <section class="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
+        <section className="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
         <h1>404</h1>
         <h2>The page you are looking for doesn't exist.</h2>
-        <a class="btn" href="/">Back to home</a>
-        <img src="assets/img/not-found.svg" class="img-fluid py-5" alt="Page Not Found" />
+        <a className="btn" href="/">Back to home</a>
+        <img src={ window.location.origin + "/assets/img/not-found.svg" } className="img-fluid py-5" alt="Page Not Found" />
       </section>
     </>
 };
